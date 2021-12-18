aaf19b54cc7c65169c2833d109e3002739d1549e
Arvind-4 <lordarvindthegamer@gmail.com>
2021-12-18 16:26:17 +0530

    Changed url and test

--- posts/tests.py
+++ posts/tests.py
@@ -12,9 +12,9 @@ class BlogPostTestCase(TestCase):
         self.client = Client()
 
     def test_get_post_list_view(self, *args, **kwargs):
-        response = self.client.get(reverse('list_view'))
+        response = self.client.get(reverse('list_view'), {}, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_tags_list_view(self, *args, **kwargs):
-        response = self.client.get(reverse('tags_list'))
+        response = self.client.get(reverse('tags_list'), {}, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
