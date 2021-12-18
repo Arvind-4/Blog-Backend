568407e1039c04d6772ecdc967974273f46c0478
Arvind-4 <lordarvindthegamer@gmail.com>
2021-12-18 22:28:48 +0530

    added env in base.py

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
