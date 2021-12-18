f583b245b947e570aaa2e3dce56d51cf84b28eac
Arvind-4 <lordarvindthegamer@gmail.com>
2021-12-18 22:31:01 +0530

    configure workflow

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -124,8 +123,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-if '/app' in os.environ['HOME']:
-    import django_heroku
-    django_heroku.settings(locals())
