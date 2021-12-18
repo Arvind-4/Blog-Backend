f252e5e6e3025b24dc748ab38e711be23f3e2f3f
Arvind-4 <lordarvindthegamer@gmail.com>
2021-12-18 22:25:10 +0530

    removed sys in base.py

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -73,13 +73,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if sys.argv[1] == 'test':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
